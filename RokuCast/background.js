6b1245b1903fdb5fd7b939845ad9b123a9325efc
Pranav Prakash <pranav-prakash@users.noreply.github.com>
2017-07-14 21:38:34 -0700

    Merge in redirect resolve
     from the playonroku branch

--- RokuCast/background.js
+++ RokuCast/background.js
@@ -19,16 +19,21 @@ function openApp(msg)
             console.log(tabs);
             console.log(title);
             console.log(favIconURL);
+            
+            var xhr = new XMLHttpRequest();
+            xhr.open('HEAD', msg.sentLink, true);
+            xhr.onload = function () {
+                var url = "http://" + ip + ":8060/launch/dev?version=1" + "&url=" + encodeURIComponent(xhr.responseURL) + "&title=" + encodeURIComponent(title) + "&image=" + encodeURIComponent(favIconURL);
+                var method = "POST";
+                var postData = "";
+                var async = true;
 
-            var url = "http://" + ip + ":8060/launch/dev?version=1" + "&url=" + encodeURIComponent(msg.sentLink) + "&title=" + encodeURIComponent(title) + "&image=" + encodeURIComponent(favIconURL);
-            var method = "POST";
-            var postData = "";
-            var async = true;
-
-            var request = new XMLHttpRequest();
-            request.open(method, url, async);
-            request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-            request.send(postData);
+                var request = new XMLHttpRequest();
+                request.open(method, url, async);
+                request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+                request.send(postData);
+            };
+            xhr.send(null);
         })
     }
     else
