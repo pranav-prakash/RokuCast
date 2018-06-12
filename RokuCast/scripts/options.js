2dfba7cf44cee8394fa7857e2d3ae18d9da480fb
Josh Howe <howejosh@gmail.com>
2018-06-12 13:29:55 -0400

    Format options.{html,js} for consistency

--- RokuCast/scripts/options.js
+++ RokuCast/scripts/options.js
@@ -1,20 +1,19 @@
 // Saves options to chrome.storage
 function save_options() {
-  var ip_address = document.getElementById('ip_address').value;
-  localStorage["ipAddress"] = ip_address;
+    var ip_address = document.getElementById('ip_address').value;
+    localStorage['ipAddress'] = ip_address;
     var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
+    status.classList.remove('hidden');
     setTimeout(function() {
-      status.textContent = '';
+        status.classList.add('hidden');
     }, 750);
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  document.getElementById('ip_address').value = (localStorage["ipAddress"] == undefined) ? "" : localStorage["ipAddress"];
+    document.getElementById('ip_address').value = (localStorage['ipAddress'] == undefined) ? '' : localStorage['ipAddress'];
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+document.getElementById('save').addEventListener('click', save_options);
